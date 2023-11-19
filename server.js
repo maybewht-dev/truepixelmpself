4463430fbcef12da1196295d5391198422ba534c
dmitry <121082368+maybewht-dev@users.noreply.github.com>
2023-11-19 23:20:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:8080/sokiosk', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/sokiosk', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,

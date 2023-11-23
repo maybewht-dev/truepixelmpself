ebfe9fc9f5c4669cb777cf869566ceea4f45ccb1
dmitry <121082368+maybewht-dev@users.noreply.github.com>
2023-11-23 19:19:28 +0300

    Update OrderScreen.js

--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -94,7 +94,7 @@ export default function OrderScreen(props) {
           maxWidth="sm"
         >
           <DialogTitle className={styles.center}>
-            Add {product.name}
+            {product.name}
           </DialogTitle>
           <Box className={[styles.row, styles.center]}>
             <Button
